c552231d5e1fdeeee58df0432e7765458847ce60
Sylvain Romiguier <sylvain.romiguier@gmail.com>
2025-04-23 17:39:11 +0200

    chore(test): automatic deploy

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { UserProviderJsonPlaceHolder } from './SPI/User.provider.jsonPlaceHolder';
-import { Store } from './Store.service';
-import { UserUseCases } from './domain/User.useCases';
-import './style.css';
+import { UserProviderJsonPlaceHolder } from "./SPI/User.provider.jsonPlaceHolder";
+import { Store } from "./Store.service";
+import { UserUseCases } from "./domain/User.useCases";
+import "./style.css";
 
 // Register the providers and use cases
-Store.services().provide('userProvider', new UserProviderJsonPlaceHolder());
-Store.services().provide('userUseCases', new UserUseCases());
+Store.services().provide("userProvider", new UserProviderJsonPlaceHolder());
+Store.services().provide("userUseCases", new UserUseCases());
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="app-container">
     <div class="top-bar">
-    <h1>Back to Basics : a minimal frontend framework</h1>
+    <h1>Back to Basics : a minimal frontend framework new version</h1>
     <h2>Dependency Injection and reactive Web Components in Typescript</h2>
     <app-switch>In Memory Provider</app-switch>
     </div>
